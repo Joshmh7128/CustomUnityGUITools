c91c449d324fdfc2fbcae79612bd72fc91e39373
Josh <35745910+Joshmh7128@users.noreply.github.com>
2022-12-01 22:53:54 -0500

    Added Random Rotation Buttons, Added Camera Button, Added FPS Readout

---
+++ CustomEditorTools/Assets/Scripts/CustomTools.cs
@@ -0,0 +1,72 @@
+// Editor script that lets you scale the selected GameObject between 1 and 100
+
+using UnityEditor;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class CustomTools : EditorWindow
+{
+    static float xscale = 0.0f;
+    static float yscale = 0.0f;
+    static float zscale = 0.0f;
+
+    [MenuItem("Custom Tools/ExampleTools")]
+    // The Init function is used to show a new editor window
+    static void Init()
+    {
+        EditorWindow window = GetWindow(typeof(CustomTools));
+        window.Show();
+    }
+
+    void OnGUI()
+    {
+        // write a header
+        EditorGUILayout.LabelField("Resize Selected Object", EditorStyles.boldLabel);
+        // show our sliders
+        xscale = EditorGUILayout.Slider(xscale, 1, 100);
+        yscale = EditorGUILayout.Slider(yscale, 1, 100);
+        zscale = EditorGUILayout.Slider(zscale, 1, 100);
+
+        // time since editor start
+        EditorGUILayout.LabelField("Editor Session Time: ", Time.time.ToString(), EditorStyles.boldLabel);        
+        // our fps
+        EditorGUILayout.LabelField("Editor FPS: ", ((int)1f /Time.deltaTime).ToString(), EditorStyles.boldLabel);
+
+        // button
+        EditorGUILayout.LabelField("Button Examples", EditorStyles.boldLabel);
+
+        if (GUILayout.Button("Randomly Rotate Object"))
+        {
+            // randomly rotate our current object
+            Selection.activeTransform.eulerAngles = new Vector3(Random.Range(-180, 180), Random.Range(-180, 180), Random.Range(-180, 180));
+        }
+
+        if (GUILayout.Button("Reset Object Rotation"))
+        {
+            // randomly rotate our current object
+            Selection.activeTransform.eulerAngles = Vector3.zero;
+        }
+        
+        if (GUILayout.Button("Create Camera from View"))
+        {
+            // find our object in space
+            Quaternion camRot = SceneView.lastActiveSceneView.rotation;
+            Vector3 camPos = SceneView.lastActiveSceneView.camera.transform.position;
+            // create a new camera in that position
+            Camera ourCam = Instantiate(new GameObject(), camPos, camRot).AddComponent<Camera>();
+            // remove extra object
+            DestroyImmediate(GameObject.Find("New Game Object"));
+
+            ourCam.fieldOfView = SceneView.lastActiveSceneView.camera.fieldOfView;
+            ourCam.gameObject.name = "Custom Created Camera";
+        }
+
+        Repaint();
+    }
+
+    void OnInspectorUpdate()
+    {
+        if (Selection.activeTransform)
+            Selection.activeTransform.localScale = new Vector3(xscale, yscale, zscale);
+    }
+}
